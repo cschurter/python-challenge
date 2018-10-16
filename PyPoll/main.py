1537b03c10a84a4b47af86ce8d03fa19b271ee59
student x <carolineschurter@yahoo.com>
2018-10-16 11:46:00 -0700

    finished HW

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -26,20 +26,8 @@ with open (csvPoll, newline="") as csvfile:
 #create file pointer for write
 with open(outFile,"w") as txtfile:
 
-    #print header
-    print("Election Results")
-    print("-----------------------------")
-
-    print("Election Results",file=txtfile)
-    print("-----------------------------",file=txtfile) 
-
     #total votes
     totalVotes = len(rows)
-    print(f'Total Votes: {totalVotes}')
-    print(f'Total Votes: {totalVotes}',file=txtfile)
-    
-    print("-----------------------------") 
-    print("-----------------------------",file=txtfile) 
 
     for row in rows:
         if(row[2] in votes): #if key row[2] exists in votes dictionary
@@ -47,16 +35,50 @@ with open(outFile,"w") as txtfile:
         else:
             votes[row[2]] =1
     #print(votes) #prints total votes for each candidate
-   
-    
-    for key in votes.keys():
-        print(f'{key}:{votes[key]}') 
-        print(f'{key}:{votes[key]}',file=txtfile)
-    
+       
     #to get percentage of votes each candidate won    
-  
-        
+    def getPercentage(votes):
+        return (votes/totalVotes)
 
+    #print header
+    print("Election Results")
+    print("-----------------------------")
+
+    print("Election Results",file=txtfile)
+    print("-----------------------------",file=txtfile) 
+
+    print(f'Total Votes: {totalVotes}')
+    print(f'Total Votes: {totalVotes}',file=txtfile)
     
+    print("-----------------------------") 
+    print("-----------------------------",file=txtfile) 
+    
+    keys=list(votes.keys())
+    counts=list(votes.values())
 
+    khanVotes = votes['Khan']
+    correyVotes = votes['Correy']
+    liVotes = votes['Li']
+    otooleyVotes = votes["O'Tooley"]
 
+    print(f'{keys[0]}: {getPercentage(khanVotes):.3%} ({counts[0]})')
+    print(f'{keys[0]}: {getPercentage(khanVotes):.3%} ({counts[0]})', file=txtfile)
+    
+    print(f'{keys[1]}: {getPercentage(correyVotes):.3%} ({counts[1]})')
+    print(f'{keys[1]}: {getPercentage(correyVotes):.3%} ({counts[1]})', file=txtfile)
+    
+    print(f'{keys[2]}: {getPercentage(liVotes):.3%} ({counts[2]})')
+    print(f'{keys[2]}: {getPercentage(liVotes):.3%} ({counts[2]})', file=txtfile)
+    
+    print(f'''{keys[3]}: {getPercentage(otooleyVotes):.3%} ({counts[3]})''')
+    print(f'''{keys[3]}: {getPercentage(otooleyVotes):.3%} ({counts[3]})''', file=txtfile)
+    
+    print("-----------------------------") 
+    print("-----------------------------",file=txtfile) 
+
+    indxWinner = counts.index(max(counts))
+    print(f'Winner: {keys[indxWinner]}')
+    print(f'Winner: {keys[indxWinner]}',file=txtfile)
+
+    print("-----------------------------") 
+    print("-----------------------------",file=txtfile)
